c34d5596be8e1ca602e79f2269993b4d53da48e4
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2020-12-20 22:18:48 +0100

    Napravljeni projekti.

---
+++ Let/src/main/java/SK/Let/LetApplication.java
@@ -0,0 +1,13 @@
+package SK.Let;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetApplication.class, args);
+	}
+
+}
---
+++ Let/src/test/java/SK/Let/LetApplicationTests.java
@@ -0,0 +1,13 @@
+package SK.Let;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
