a978ade269fbcd8ffdf1b964ecccec486b01e0ef
lkovacevic1 <lukablok4598@gmail.com>
2020-12-20 22:06:03 +0100

    Kreiran service za letove

---
+++ Let/src/main/java/app/LetApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetApplication.class, args);
+	}
+
+}
