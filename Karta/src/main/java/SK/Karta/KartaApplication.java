3c0ef43e9d01c9eb7706c2764babee3217a7e054
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2020-12-20 22:06:20 +0100

    commit.

---
+++ Karta/src/main/java/SK/Karta/KartaApplication.java
@@ -0,0 +1,13 @@
+package SK.Karta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartaApplication.class, args);
+	}
+
+}
