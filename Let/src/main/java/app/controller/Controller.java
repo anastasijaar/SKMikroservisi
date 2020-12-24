b0c23f3b42a14b13e60e3be066de197d4df35688
lkovacevic1 <lukablok4598@gmail.com>
2020-12-24 19:39:34 +0100

    Dodato izlistavanje svih letova kod kojih kapacitet putnika nije pun

--- Let/src/main/java/app/controller/Controller.java
+++ Let/src/main/java/app/controller/Controller.java
@@ -1,24 +1,32 @@
 package app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Avion;
+import app.entities.Let;
 import app.forms.Avion_Form;
 import app.repository.AvionRepository;
+import app.repository.LetRepository;
 
 @RestController
 public class Controller {
 	
 	private AvionRepository avionRepo;
 	
+	private LetRepository letRepo;
+	
 	@Autowired
-	public Controller(AvionRepository avionRepo) {
+	public Controller(AvionRepository avionRepo, LetRepository letRepo) {
 		this.avionRepo = avionRepo;
+		this.letRepo = letRepo;
 	}
 	
 	@PostMapping("/avion")
@@ -32,6 +40,23 @@ public class Controller {
 			return new ResponseEntity<>("success", HttpStatus.OK);
 		}catch (Exception e) {
 			System.out.println("error 123");
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping("/spisakLetova")
+	public ResponseEntity<String> spisakLetova(){
+		
+		try {
+			
+			List<Let> let = letRepo.selectAllFlightWithFreeSpace();
+			for (Let l : let) {
+				System.out.println(l.getPocetnaDestinacija() + " " + l.getKrajnjaDestinacija());
+			}
+			
+			return new ResponseEntity<>("sve ok", HttpStatus.OK);
+		}catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
--- Let/src/main/java/app/repository/LetRepository.java
+++ Let/src/main/java/app/repository/LetRepository.java
@@ -1,5 +1,7 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,10 @@ public interface LetRepository extends JpaRepository<Let, Integer>{
 	//Selekcija leta po imenu aviona
 	@Query("select l from Let l where l.avion.nazivAviona like :ime")
 	Let selectFlightByPlaneName(String ime);
+	
+	//Selekcija letova gde kapacitet putnika u avionu nije pun
+	@Query("select l from Let l where l.avion.kapacitet not in l.avion.trenutnoPutnika")
+	List<Let> selectAllFlightWithFreeSpace(); 
 	
 	Let findByIdLeta(int idLeta);
 }
